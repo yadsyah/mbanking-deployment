36383fdd51bb287965e32cff1bec1dce5175c5f1
yadsyah <diyansetiyadi@gmail.com>
2022-12-31 22:22:26 +0700

    feat: add deploy docker-compose

---
+++ docker/nginx/auth/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func checkAuth(w http.ResponseWriter, r *http.Request) {
+	authString := r.Header.Get("Authorization")
+	if authString == "mbanking-dian-setiyadi" {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Authorized: true"))
+		return
+	}
+	http.Error(w, "Authorized: false", http.StatusUnauthorized)
+}
+
+func main() {
+	http.HandleFunc("/authenticated", checkAuth)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
